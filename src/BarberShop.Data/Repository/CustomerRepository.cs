47724132cea231e81baba22dbae92cdc5fbaea52
marcos <msaorc@hotmail.com>
2025-04-01 22:59:26 -0300

    Estrutura inicial do projeto BarberShop

---
+++ src/BarberShop.Data/Repository/CustomerRepository.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace BarberShop.Data.Repository;
+
+public class CustomerRepository : Repository
+{
+    
+}
---
+++ src/BarberShop.Data/Repository/Repository.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace BarberShop.Data.Repository;
+
+public class Repository
+{
+}
